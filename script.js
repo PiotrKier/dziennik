a556a406c0866082caad92358c051dc8945d2e3f
PiotrKier <113298589+PiotrKier@users.noreply.github.com>
2024-12-06 21:37:30 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function goto_logowanie_nauczyciel() {
+    window.location.href = "logowanie_nauczyciel.php";
+}
+function goto_logowanie_uczen() {
+    window.location.href = "logowanie_uczen.php";
+}
