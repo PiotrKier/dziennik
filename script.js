d97e114c2b3dd249da09a3eedfbda218ff3d2308
PiotrKier <113298589+PiotrKier@users.noreply.github.com>
2024-12-08 01:28:21 +0100

    Delete script.js

--- script.js
+++
@@ -1,6 +0,0 @@
-function goto_logowanie_nauczyciel() {
-    window.location.href = "logowanie_nauczyciel.php";
-}
-function goto_logowanie_uczen() {
-    window.location.href = "logowanie_uczen.php";
-}
